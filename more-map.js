246804a48e4f02f3075f966e89d19ecc774f4e8e
rased100 <rased100@gmail.com>
2022-01-13 22:24:43 +0600

    first commit

---
+++ more-map.js
@@ -0,0 +1,11 @@
+const friends = ['tased', 'rased', 'tusar', 'dulal', 'julfikar'];
+
+const fLength = friends.map(x => x.length);
+console.log(fLength);
+
+
+
+const friends2 = ['Tom Hanks', 'Tom Cruise', 'Tom Brady', 'Tom Solaiman'];
+
+const fLengths2 = friends2.map(friend => friend.length);
+console.log(fLengths2);
